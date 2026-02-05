2b63d0ddd41e18dd8c1e415bbe9efe742122274b
WANG Zhuo <zwang2346@ad.cityu.edu.hk>
2026-02-05 18:43:39 +0800

    add

---
+++ project_repos/get_repos.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# The name of the archive that contains all project repos
+ARCHIVE=defects4j-repos.zip
+
+clean() {
+    rm -rf \
+    closure-compiler.git \
+    commons-lang.git \
+    commons-math.git \
+    jfreechart \
+    joda-time.git \
+    README 
+}
+
+# The BSD version of stat does not support --version or -c
+if stat --version &> /dev/null; then
+    # GNU version
+    cmd="stat -c %Y $ARCHIVE"
+else
+    # BSD version
+    cmd="stat -f %m $ARCHIVE"
+fi
+
+if [ -e $ARCHIVE ]; then
+    old=$($cmd)
+else
+    old=0
+fi
+# Only download repos if the server has a newer file
+wget -N https://defects4j.org/downloads/$ARCHIVE
+new=$($cmd)
+
+# Exit if no newer file is available
+[ "$old" == "$new" ] && exit 0
+
+# Remove old files
+clean
+
+# Extract new repos
+unzip -q -u $ARCHIVE && mv defects4j/project_repos/* . && rm -r defects4j
